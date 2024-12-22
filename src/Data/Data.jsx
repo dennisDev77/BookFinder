440f3edc9a9e192dad11faeb6ce898e273b69199
dennisDev77 <diniscamela5@gmail.com>
2024-12-22 15:51:03 +0100

    update

--- src/Data/Data.jsx
+++ src/Data/Data.jsx
@@ -1,22 +1,24 @@
-import { TbCategoryFilled } from "react-icons/tb";
 import { IoHeart } from "react-icons/io5";
-import { GiMaterialsScience } from "react-icons/gi";
-import { LiaFantasyFlightGames } from "react-icons/lia";
-import { MdChangeHistory } from "react-icons/md";
-import { GiLoveMystery } from "react-icons/gi";
 
 export const menu=[
     {
         id:1,
-        title:'Categoria',
-        iconMain:<TbCategoryFilled/>,
-        aba1:'Romance',
-        aba2:'Ficcao Cientifica',
-        aba3:'Fantasia',
-        aba4:'Historia',
-        aba5:'Misterio',
-        btn:'Meus Favoritos',
-        btnIcon:<IoHeart/>,
-
+        aba:'Romance',
+    },
+    {
+        id:2,
+        aba:'Ficcao Cientifica',
+    },
+    {
+        id:3,
+        aba:'Fantasia',
+    },
+    {
+        id:4,
+        aba:'Historia',
+    },
+    {
+        id:5,
+        aba:'Misterio',
     }
 ]
--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -3,65 +3,69 @@ import { FiBookOpen } from "react-icons/fi";
 import { LuMenu } from "react-icons/lu";
 import { IoClose } from "react-icons/io5";
 import { LiaSearchSolid } from "react-icons/lia";
+import { TbCategoryFilled } from "react-icons/tb";
 import { Link } from 'react-router-dom';
 import {menu} from '../Data/Data'
 import styles from './Header.module.css'
 
 const Header = () => {
   const [menuState, setMenuState]=React.useState(false)
+
   return (
+
     <header className='flex items-center shadow-sm'>
         {/* Add logotipo book-open */}
-        <div className='flex justify-between w-full py-2 px-10 items-center'>
+    <div className='flex justify-between w-full py-2 px-10 items-center'>
 
     {/* Colocando a logo */}
       <Link>
         <h1 className=' text-5xl text-color-blue flex items-center justify-center gap-2'> <span><FiBookOpen/></span> <span className='text-4xl text-color-black font-medium'>BookFinder</span></h1>
       </Link>
-  {/* Fim */}
+    {/* Fim */}
 
     <div className='flex justify-between items-center gap-16'>
-  {/*  Input search */}
-      <div className={`${styles.inputSearch} w-80 flex items-center relative`}>
-      <input type="search" placeholder='Procure por: titulo, autor ou genero' className=' w-full transition p-3 px-4 text-sm text-color-black rounded-full'/>
+    {/*  Input search */}
 
-      <button className=' absolute right-2 text-2xl p-2 text-color-gray bg-white'><LiaSearchSolid/></button>
+      <div className={`${styles.inputSearch} w-80 flex items-center relative`}>
+      <input type="search" placeholder='Procure por: titulo, autor ou genero' className=' w-full transition p-3   px-4 text-sm text-color-black rounded-full bg-color-white'/>
+
+      <button className=' absolute right-2 text-2xl p-2 text-color-black bg-color-white'>
+        <LiaSearchSolid/>
+      </button>
       </div>
 
-    {/* Mostrando menu Icon */}
+     {/* Mostrando menu Icon */}
       <div>
         <button className='text-3xl' onClick={()=>setMenuState(!menuState)}> {menuState ? <IoClose/> : <LuMenu/> }</button>
       </div>
          
-         { menuState&& menu.map(menu=>(
-         <div className={`${styles.menuWrapper} shadow-md flex flex-col justify-center fixed top-16 right-1 gap-3 p-6`} key={menu.id}>
+       {menuState &&
+        <div className={`${styles.menuWrapper} shadow-md flex flex-col justify-center fixed top-16 right-1 gap-3 p-6`}>
 
-            <div className=' flex items-center gap-1 text-color-black text-xl'>
-              <span className='text-2xl'>{menu.iconMain}</span>
-              <span>{menu.title}</span>
-            </div>
+          <div className=' flex items-center gap-1 text-color-black text-xl'>
+            <span className='text-2xl'><TbCategoryFilled/></span>
+            <span>Categorias</span>
+          </div>
 
-            {/* Menu list */}
-            <div className='menus flex flex-col justify-center text-base gap-1'>
-            <Link className={`${styles.abaMenu} shadow-sm text-color-gray`}>{menu.aba1}</Link>
-            <Link className={`${styles.abaMenu} shadow-sm text-color-gray`}>{menu.aba2}</Link>
-            <Link className={`${styles.abaMenu} shadow-sm text-color-gray`}>{menu.aba3}</Link>
-            <Link className={`${styles.abaMenu} shadow-sm text-color-gray`}>{menu.aba4}</Link>
-            <Link className={`${styles.abaMenu} shadow-sm text-color-gray`}>{menu.aba5}</Link>
-            </div>
+          {/* Menu list */}
 
-            {/* Btn Favoritos */}
-            <Link className=' flex justify-center items-center gap-1 bg-color-blue p-1 px-4 text-color-white rounded-sm'>
-              <span className='text-center'>{menu.btn}</span>
-              {/* <span className='text-center'>{menu.btnIcon}</span> */}
-            </Link>
+        { menu.map(menu=>(
+          <div className='menus flex flex-col justify-center text-base gap-1'  key={menu.id}>
+          <Link className={`${styles.abaMenu} shadow-sm text-color-gray`}>{menu.aba}</Link>
+          </div>
 
-         </div>
-         ))}
+      ))}
+          {/* Btn Favoritos */}
+          <Link className=' flex justify-center items-center gap-1 bg-color-blue p-1 px-4 text-color-white rounded-sm'>
+            <span className='text-center'>Meus Favoritos</span>
+            {/* <span className='text-center'>{menu.btnIcon}</span> */}
+          </Link>
 
-         </div>
+        </div>}
 
-      </div>
+    </div>
+
+    </div>
     </header>
   )
 }
