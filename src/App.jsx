8337b2e73101bfba0d9d3f37810f513e88f3a258
dennisDev77 <diniscamela5@gmail.com>
2024-12-22 11:37:51 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import './App.css'
 import Header from './Header/Header'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './Home/Home'
 
 function App() {
   return (
     <>
-         <section className='container'>
+         <section>
+
+          <BrowserRouter>
           <Header/>
+
+          <main className='container'>
+           <Routes>
+              <Route path='/' element={<Home/>}/>
+          </Routes>  
+
+          </main>
+          </BrowserRouter>
+
         </section>
     </>
   )
--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -2,23 +2,29 @@ import React from 'react'
 import { FiBookOpen } from "react-icons/fi";
 import { IoIosMenu } from "react-icons/io";
 import { IoClose } from "react-icons/io5";
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <header className='flex items-center'>
+    <header className='flex items-center shadow-sm'>
         {/* Add logotipo book-open */}
-        <div className='flex justify-between w-full'>
-      <div>
-        <h1 className=' text-4xl text-color-blue'><FiBookOpen/></h1>
+        <div className='flex justify-between w-full py-2 px-10 items-center'>
+
+      <Link>
+        <h1 className=' text-5xl text-color-blue'><FiBookOpen/></h1>
+      </Link>
+
+<div className='flex justify-between items-center gap-20'>
+      <div className='border w-64 '>
+      <input type="search" placeholder='Procure por: titulo, autor ou ' className='w-full p-1 rounded-full' />
       </div>
 
       <div>
-        <input type="search" placeholder='Procure por: titulo, autor ou ' />
-      </div>
-      <div>
-        <button> <IoIosMenu/> <IoClose/></button>
+        <button className='text-2xl'> <IoIosMenu/></button>
+
       </div>
       </div>
+    </div>
     </header>
   )
 }
