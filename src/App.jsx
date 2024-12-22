1773bd809bbcadaa85f987fba379b8fa92f52bfc
dennisDev77 <diniscamela5@gmail.com>
2024-12-22 12:09:32 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,6 @@ function App() {
 
           </main>
           </BrowserRouter>
-
         </section>
     </>
   )
--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { FiBookOpen } from "react-icons/fi";
-import { IoIosMenu } from "react-icons/io";
+import { LuMenu } from "react-icons/lu";
 import { IoClose } from "react-icons/io5";
 import { Link } from 'react-router-dom';
 
@@ -10,20 +10,25 @@ const Header = () => {
         {/* Add logotipo book-open */}
         <div className='flex justify-between w-full py-2 px-10 items-center'>
 
+    {/* Colocando a logo */}
       <Link>
-        <h1 className=' text-5xl text-color-blue'><FiBookOpen/></h1>
+        <h1 className=' text-5xl text-color-blue flex items-center justify-center gap-2'> <span><FiBookOpen/></span> <span className='text-4xl text-color-black font-medium'>BookFinder</span></h1>
       </Link>
+{/* Fim */}
 
-<div className='flex justify-between items-center gap-20'>
+    <div className='flex justify-between items-center gap-20'>
+  {/*  Input search */}
       <div className='border w-64 '>
-      <input type="search" placeholder='Procure por: titulo, autor ou ' className='w-full p-1 rounded-full' />
+      <input type="search" placeholder='Procure por: titulo, autor ou ' className='w-full p-1 rounded-full'/>
       </div>
 
+    {/* Mostrando menu Icon */}
       <div>
-        <button className='text-2xl'> <IoIosMenu/></button>
+        <button className='text-3xl'> <LuMenu/></button>
+      </div>
 
       </div>
-      </div>
+
     </div>
     </header>
   )
